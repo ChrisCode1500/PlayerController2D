e2c1fcf9ed88cc3a13751fedbc343bdc9da56a58
ChrisCode1500 <45640544+ChrisCode1500@users.noreply.github.com>
2019-11-19 18:02:46 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private bool facingRight;
+    
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+
+    private Rigidbody2D rb2d;
+
+    float fJumpPressedRemember = 0;
+    [SerializeField]
+    float fJumpPressedRememberTime = 0.2f;
+
+    float fGroundedRemember = 0;
+    [SerializeField]
+    float fGroundedRememberTime = 0.25f;
+
+    [Range(0, 1)]
+    float fCutJumpHeight = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb2d = GetComponent<Rigidbody2D>();
+    }  
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+        
+        if (facingRight == false && moveInput < 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        Jump();
+        RememberJump();
+    }
+
+    // This is a way to flip the character to face the direction you are moving in. An easier solution would be SpriteRenderer.flipX = True/False; so look into this
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);   
+    }
+
+    void Jump()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        // Allows the player to jump if they have any extra jumps left.        
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+        }        
+
+        // Implements a less floaty feel to jumping
+        if (rb2d.velocity.y < 0)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; // The - 1 is accounting for the physics systems normal gravity
+        }
+        else if (rb2d.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void RememberJump()
+    {
+        // Start Testing code
+
+        fGroundedRemember -= Time.deltaTime;
+        if (isGrounded)
+        {
+            fGroundedRemember = fGroundedRememberTime;
+        }
+
+        fJumpPressedRemember -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            fJumpPressedRemember = fJumpPressedRememberTime;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (rb2d.velocity.y > 0)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * fCutJumpHeight);
+            }
+        }
+
+        if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
+        {
+            fJumpPressedRemember = 0;
+            fGroundedRemember = 0;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+
+        // End Testing code
+    }
+}
